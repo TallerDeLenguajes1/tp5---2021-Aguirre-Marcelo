301ef91b3a7d9799aa66bbfd2d2f5e2891f34d5f
Aguirre Marcelo <exadalx@gmail.com>
2021-06-04 12:39:45 -0300

    Punto1 terminado.

--- TP5/TP5/Program.cs
+++ TP5/TP5/Program.cs
@@ -6,7 +6,48 @@ namespace TP5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Punto1();
+            Punto2();
+            Punto3();
+        }
+        public static void Punto1()
+        {
+            int aux, numero;
+            int inverso = 0;
+            string cadena;
+
+            Console.WriteLine("ingrese un numero : ");
+            cadena = Console.ReadLine();
+
+            numero = Convert.ToInt32(cadena);
+
+            if (numero > 0)
+            {
+                while (numero > 0)
+                {
+                    aux = numero % 10;
+                    numero = numero / 10;
+                    inverso = inverso * 10 + aux;
+                }
+
+                Console.WriteLine("El numero ingresado, al invertirlo queda como: " + inverso);
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es menor a 1, no se invertira.");
+            }
+
+        }
+        public static void Punto2()
+        {
+            int numeroA, numeroB;
+
+
+            
+        }
+        public static void Punto3()
+        {
+
         }
     }
 
